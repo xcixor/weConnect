c44c99e6c4867eeda19601db1dc9aed3bdd65053
Peter <pndungu54@gmail.com>
2018-03-01 15:29:27 +0300

    Feature: Create navbar
    A top navbar will ease access to other pages.

--- designs/UI/static/js/materialize_init.js
+++ designs/UI/static/js/materialize_init.js
@@ -6,3 +6,4 @@ $(document).ready(function(){
 // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
 $('.modal').modal();
 });
+$(".dropdown-button").dropdown();
