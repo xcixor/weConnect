c9960252664799897e3c74fd47df7ed8a9362585
Peter <pndungu54@gmail.com>
2018-03-06 10:21:12 +0300

    [Chore: #155690980] Add tests for user class

---
+++ tests/test_business.py
@@ -0,0 +1,95 @@
+"""Contains test for the Business class"""
+
+import unittest
+
+from app.api_v1.models import Business, Review
+
+class TestBusinessCase(unittest.TestCase):
+    """Test Business class functionality
+    Attributes:
+        business(obj): a business object to be used for subsequent tests
+    Methods:
+        test_create_business()
+        test_cannot_create_duplicate_businesses()
+        test_create_business()
+        test_edit_business()
+        test_delete_business()
+        test_find_business_by_id()
+        test_find_businesses_by_category()
+        test_find_businesses_by_location()
+        test_write_review()
+        test_get_all_reviews()
+    """
+    def setUp(self):
+        self.mock_business = Business("James Barber", "We sell bananas", "Kawangware", "fruit vendors")
+        self.success_user = User('ptah', 'pndungu54@gmail.com', 'pass123', 'pass123')
+        
+    def tearDown(self):
+        del self.mock_business
+
+    def test_create_business(self):
+        """Test whether a business can be created succesfully"""
+        response = self.mock_business.create_business()
+        self.assertTrue(response)
+        self.assertEqual(Business.get_all_businesses(), [{"Name":"James Barber", "Descrition":"We sell bananas", "Location":"Kawangware", "Category":"fruit vendors"}])
+    
+    def test_cannot_create_duplicate_businesses(self):
+        """Test app cannot duplicate business"""
+        self.mock_business.create_business()
+        another_biz = Business("james barber", "We sell bananas", "Kawangware", "fruit vendors")
+        response = another_biz.create_business()
+        self.assertEqual(response['message'], \
+        'Cannot create duplicate business')
+
+    def test_edit_business(self):
+        """Test app can edit a business successfully"""
+        self.mock_business.edit_business()
+        response = self.mock_business.edit_business(Description='We no longer sell bananas')
+        self.assertTrue(response)
+        self.assertEqual(Business.get_all_businesses(), [{"Name":"James Barber", "Description":"We no longer sell bananas", "Location":"Kawangware", "Category":"fruit vendors"}])
+
+    def test_delete_business(self):
+        """Test app can delete business successfully"""
+        self.mock_business.create_business()
+        response = self.mock_business.delete_business()
+        self.assertTrue(response)
+        self.assertEqual(Business.get_all_businesses(), [])
+
+    def test_find_event_by_id(self):
+        """Test app can find a business by id"""
+        self.mock_business.create_business()
+        response = Business.get_business_by_id(1)
+        self.assertEqual(response, {"Name":"James Barber", "Descrition":"We sell bananas", "Location":"Kawangware", "Category":"fruit vendors"})
+
+    def test_find_businesses_by_category(self):
+        """Test app can find a businesses by category"""
+        self.mock_business.create_business()
+        another_biz = Business("jane's fruits", "All kinds of fruits", "Kihanya", "fruit vendors")
+        another_biz.create_business()
+        response = Business.get_businesses_by_category()
+        self.assertEqual(response, [{"Name":"James Barber", "Description":"We sell bananas", "Location":"Kawangware", "Category":"fruit vendors"},
+                        {"Name":"jane's fruits", "Description":"All kinds of fruits", "Location":"Kihanya", "Category":"fruit vendors"}])
+
+    def test_find_businesses_by_location(self):
+        """Test app can find a businesses by category"""
+        self.mock_business.create_business()
+        another_biz = Business("juma cafe", "Mandazi moto stop", "Kawangware", "food")
+        another_biz.create_business()
+        response = Business.get_businesses_by_category()
+        self.assertEqual(response, [{"Name":"James Barber", "Description":"We sell bananas", "Location":"Kawangware", "Category":"fruit vendors"},
+                        {"Name":"juma cafe", "Description":"Mandazi moto stop", "Location":"Kawangware", "Category":"food"}])
+        
+
+    def test_add_business_review(self):
+        """Test user can write a business review"""
+        self.mock_business.create_business()
+        response = self.mock_business.write_review('Loved the place', self.success_user.username)
+        self.assertEqual(response, 'Review written successfuly')
+
+    def test_get_all_reviews(self):
+        """Test app can get all reviews for a particular business"""
+        self.mock_business.create_business()
+        self.mock_business.write_review('Worst services ever')
+        self.mock_business.write_review('Impunity of the highest order')
+        response = self.mock_business.get_all_reviews()
+        self.assertEqual(response, [{'id':'1', 'comment':'Worst services ever'}, {'id':'2', 'comment':'Impunity of the highest order'}])
