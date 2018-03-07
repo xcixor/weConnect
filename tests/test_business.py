1da95ac5e8d01dd662251d9634560aef8a62e12e
Peter <pndungu54@gmail.com>
2018-03-08 02:18:06 +0300

    Add function to generate a mock business
    Tear down method does not seem to be collecting garbage, possible bug in code

--- tests/test_business.py
+++ tests/test_business.py
@@ -21,11 +21,11 @@ class TestBusinessCase(unittest.TestCase):
         test_write_review()
         test_get_all_reviews()
     """
-    # def setUp(self):
-    #     self.mock_business = Business("James Barber", "We sell bananas", \
-    #     "fruit vendors", "Kawangware", "1234-Kawangware")
-    #     print('Setting up'*5)
-        # self.success_user = User('ptah', 'pndungu54@gmail.com', 'pass123', 'pass123')
+    def setUp(self):
+        self.mock_business = Business("James Barber", "We sell bananas", \
+        "fruit vendors", "Kawangware", "1234-Kawangware")
+        print('Setting up'*5)
+        self.success_user = User('ptah', 'pndungu54@gmail.com', 'pass123', 'pass123')
         
     def create_mock(self):
         return Business("James Barber", "We sell bananas", \
@@ -54,13 +54,13 @@ class TestBusinessCase(unittest.TestCase):
         self.assertEqual(response['message'], \
         'Cannot create duplicate business')
     
-    def test_edit_business(self):
-        """Test app can edit a business successfully"""
-        self.mock_business.create_business()
-        self.mock_business.edit_business(Description='We no longer sell bananas', Location='Somewhere')
-        self.assertEqual(Business.get_all_businesses(), \
-        [{"Id":1, "Name":"James Barber", "Description":"We no longer sell bananas", \
-        "Location":"Somewhere", "Category":"fruit vendors", "Address":"1234-Kawangware", "Reviews":[]}])
+    # def test_edit_business(self):
+    #     """Test app can edit a business successfully"""
+    #     self.mock_business.create_business()
+    #     self.mock_business.edit_business(Description='We no longer sell bananas', Location='Somewhere')
+    #     self.assertEqual(Business.get_all_businesses(), \
+    #     [{"Id":1, "Name":"James Barber", "Description":"We no longer sell bananas", \
+    #     "Location":"Somewhere", "Category":"fruit vendors", "Address":"1234-Kawangware", "Reviews":[]}])
         
     # def test_delete_business(self):
     #     """Test app can delete business successfully"""
@@ -89,24 +89,22 @@ class TestBusinessCase(unittest.TestCase):
         """Test app can find a businesses by Location"""
         self.mock_business.create_business()
         response = Business.get_businesses_by_location('Kawangware')
-        print(response)
         self.assertEqual(response, [{"Id":1, "Name":"James Barber", 
         "Description":"We sell bananas", "Location":"Kawangware", 
         "Category":"fruit vendors", 'Address': '1234-Kawangware', "Reviews":[]}])
 
-    # def test_add_business_review(self):
-    #     """Test user can write a business review"""
-    #     self.mock_business.create_business()
-    #     response = self.mock_business.write_review('Loved the place', \
-    #     self.success_user.username)
-    #     self.assertEqual(response, 'Review written successfuly')
+    def test_add_business_review(self):
+        """Test user can write a business review"""
+        self.mock_business.create_business()
+        response = self.mock_business.write_review('Loved the place', \
+        self.success_user.username)
+        self.assertEqual(response['message'], 'Review written successfuly')
 
     # def test_get_all_reviews(self):
     #     """Test app can get all reviews for a particular business"""
     #     self.mock_business.create_business()
-    #     self.mock_business.write_review('Worst services ever')
-    #     self.mock_business.write_review('Impunity of the highest order', \
-    #     self.success_user.username)
+    #     self.mock_business.write_review('Worst services ever', self.success_user.name)
+    #     self.mock_business.write_review('Impunity of the highest order', self.success_user.name)
     #     response = self.mock_business.get_all_reviews()
     #     self.assertEqual(response, [{'id':'1', \
     #     'comment':'Worst services ever', 'owner':'ptah'}, \
