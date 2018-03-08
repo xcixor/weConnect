a45138c444d886fecb2c0c92ce4caa6852e48371
Peter <pndungu54@gmail.com>
2018-03-08 12:48:42 +0300

    Fix: Improve tests for better coverage

--- tests/test_business.py
+++ tests/test_business.py
@@ -2,7 +2,7 @@
 
 import unittest
 
-from app.api_v1.models import Business, User
+from app.api_1_0.models import Business, User
 
 class TestBusinessCase(unittest.TestCase):
     """Test Business class functionality
@@ -22,86 +22,78 @@ class TestBusinessCase(unittest.TestCase):
         test_get_all_reviews()
     """
     def setUp(self):
-        self.mock_business = Business("James Barber", "We sell bananas", \
-        "fruit vendors", "Kawangware", "1234-Kawangware")
-        self.success_user = User('ptah', 'pndungu54@gmail.com', 'pass123', 'pass123')
-        
-    def create_mock(self):
-        return Business("James Barber", "We sell bananas", \
-        "fruit vendors", "Kawangware", "1234-Kawangware")
+        self.comments = []
+        self.business_list = []
+        self.success_user = User('ptah', 'pndungu54@gmail.com', 'pass123', 'pass123')        
+        self.mock_business = Business(int(1), "James Barber", "We sell bananas", \
+        "fruit vendors", "Kawangware", "1234-Kawangware", self.success_user.name)
+        # self.mock_business.create_business(self.business_list)
 
     def tearDown(self):        
-        # del Business.businesses[:]
         del self.mock_business
+        del self.success_user
+        del self.business_list
 
     def test_create_business(self):
         """Test whether a business can be created succesfully"""
-        response = self.mock_business.create_business()
-        self.assertTrue(response)
-        self.assertEqual(Business.get_all_businesses(), \
-        [{"Id":1, "Name":"James Barber", "Description":"We sell bananas", \
-        "Location":"Kawangware", "Category":"fruit vendors", "Address":"1234-Kawangware", "Reviews":[]}])
+        original_length = len(self.business_list)
+        self.mock_business.create_business(self.business_list)
+        self.assertEqual(original_length + 1, len(self.business_list))
 
     def test_cannot_create_duplicate_businesses(self):
         """Test app cannot duplicate business"""
-        self.mock_business.create_business()
-        response = self.mock_business.create_business()
-        self.assertEqual(response['message'], \
-        'Cannot create duplicate business')
-    
-    # def test_edit_business(self):
-    #     """Test app can edit a business successfully"""
-    #     self.mock_business.create_business()
-    #     self.mock_business.edit_business(Description='We no longer sell bananas', Location='Somewhere')
-    #     self.assertEqual(Business.get_all_businesses(), \
-    #     [{"Id":1, "Name":"James Barber", "Description":"We no longer sell bananas", \
-    #     "Location":"Somewhere", "Category":"fruit vendors", "Address":"1234-Kawangware", "Reviews":[]}])
+        original_length = len(self.business_list)
+        self.mock_business.create_business(self.business_list)    
+        original_length = len(self.business_list)
+        self.mock_business.create_business(self.business_list)
+        self.assertEqual(original_length, len(self.business_list))          
         
-    # def test_delete_business(self):
-    #     """Test app can delete business successfully"""
-    #     self.mock_business.create_business()
-    #     response = self.mock_business.delete_business()
-    #     self.assertTrue(response)
-    #     self.assertEqual(Business.get_all_businesses(), [])
+    
+    def test_edit_business(self):
+        """Test app can edit a business successfully"""
+        self.mock_business.create_business(self.business_list)  
+        res = self.mock_business.edit_business(self.business_list, Description='We no longer sell bananas', Location='Somewhere')
+        self.assertIn('We no longer sell bananas', self.business_list[0]['Description'])
+        self.assertIn('Somewhere', self.business_list[0]['Location'])
+        
+        
+    def test_delete_business(self):
+        """Test app can delete business successfully"""
+        self.mock_business.create_business(self.business_list)  
+        response = Business.delete_business(self.business_list, 1)
+        self.assertTrue(response)
+        self.assertEqual(Business.get_all_businesses(self.business_list), [])
     
     def test_find_event_by_id(self):
         """Test app can find a business by id"""
-        self.mock_business.create_business()
-        response = Business.get_business_by_id(1)
-        self.assertEqual(response, {"Id":1, "Name":"James Barber", "Description":"We sell bananas",
-        "Location":"Kawangware", "Category":"fruit vendors", "Address":"1234-Kawangware",
-        "Reviews":[]})
+        self.mock_business.create_business(self.business_list)
+        response = Business.get_business_by_id(self.business_list, 1)
+        self.assertIn('James Barber', response['Name'])
+        
         
     def test_find_businesses_by_category(self):
         """Test app can find a businesses by category"""
-        self.mock_business.create_business()
-        response = Business.get_businesses_by_category('fruit vendors')
-        self.assertEqual(response, [{"Id":1,"Name":"James Barber",
-        "Description":"We sell bananas", "Location":"Kawangware",
-        "Category":"fruit vendors", 'Address': '1234-Kawangware', "Reviews":[]}])
+        self.mock_business.create_business(self.business_list)  
+        response = Business.get_businesses_by_category(self.business_list,'fruit vendors')
+        self.assertIn('James Barber', response[0]['Name'])
 
     def test_find_businesses_by_location(self):
         """Test app can find a businesses by Location"""
-        self.mock_business.create_business()
-        response = Business.get_businesses_by_location('Kawangware')
-        self.assertEqual(response, [{"Id":1, "Name":"James Barber", 
-        "Description":"We sell bananas", "Location":"Kawangware", 
-        "Category":"fruit vendors", 'Address': '1234-Kawangware', "Reviews":[]}])
+        self.mock_business.create_business(self.business_list)                  
+        response = Business.get_businesses_by_location(self.business_list, 'Kawangware')
+        self.assertIn('James Barber', response[0]['Name'])
 
     def test_add_business_review(self):
         """Test user can write a business review"""
-        self.mock_business.create_business()
+        self.mock_business.create_business(self.business_list)
         response = self.mock_business.write_review('Loved the place', self.success_user.name)
         self.assertEqual(response['message'], 'Review written successfuly')
 
     def test_get_all_reviews(self):
         """Test app can get all reviews for a particular business"""
-        self.mock_business.create_business()
+        self.mock_business.create_business(self.business_list)        
         self.mock_business.write_review('Worst services ever', self.success_user.name)
+        initial_length = len(self.mock_business.reviews)
         self.mock_business.write_review('Impunity of the highest order', self.success_user.name)
-        response = self.mock_business.get_all_reviews()
-        self.assertEqual(response, [{'Id':1, \
-        'Comment':'Worst services ever', 'Owner':'ptah'}, \
-        {'Id':2, 'Comment':'Impunity of the highest order', \
-        'Owner':'ptah'}])
+        self.assertEqual(initial_length + 1, 2)
     
