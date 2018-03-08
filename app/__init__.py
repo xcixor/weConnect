4d101b4a2acfc715211d339b7890d307898edac0
Peter <pndungu54@gmail.com>
2018-03-08 04:17:22 +0300

    Chore: Register api blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,10 @@ from config import config
 
 def create_app(configuration):
     """Set up and return app"""
-    app = FlaskAPI(__name__)
+    app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(config[configuration])
     config[configuration].init_app(app)
+
+    from app.api_1_0 import api as api_1_0_blueprint
+    app.register_blueprint(api_1_0_blueprint, url_prefix='/api')
     return app
