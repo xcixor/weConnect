f839ec00fa6187311f9df524657b08f2dcbc70e2
Peter <pndungu54@gmail.com>
2018-03-12 23:07:12 +0300

    Fix: Specify port during flask app call
    This will help heroku to create a web service

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 """Runs the application"""
+import os
 
 from app import create_app
 
 app = create_app('default')
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run('', port=port)
