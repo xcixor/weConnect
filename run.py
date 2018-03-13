71bf035516864fd3c5fdde1ea78cf36639648ef6
Peter <pndungu54@gmail.com>
2018-03-13 07:45:22 +0300

    Fix: Edit procfile to fix hosting issue

--- run.py
+++ run.py
@@ -6,4 +6,5 @@ from app import create_app
 app = create_app('default')
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
